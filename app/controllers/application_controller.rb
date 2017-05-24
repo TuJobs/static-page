798761f729e21c54cd017e73ec6e40fce8496b9b
Tu Jobs <nguyenvantu.epu@gmail.com>
2017-05-24 16:15:14 +0700

    edit hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception 
  
   def hello
-	render html: "xin chao !!"
+	render html: "xin chao anh tu !!"
   end
 end
